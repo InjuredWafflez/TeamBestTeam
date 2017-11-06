fe5d0ed3540d256e21974d950884f8cc5fa2a49c
rbbrown1 <rbbrown1@comcast.net>
2017-11-05 19:25:45 -0600

    Add files via upload
    Added powerups
    added underlying code for switching between different game screens
    added sounds

--- Top Down Shooter/classes.py
+++ Top Down Shooter/classes.py
@@ -1,6 +1,7 @@
 import pygame
+from math import sin, cos, pi
 
-# Base class with x and y pos and x and y vec for other classes to inherit from
+# Super class with x and y pos and x and y vec for other classes to inherit from
 class Asset(pygame.sprite.Sprite):
     def __init__(self, image = "no-image"):
         # Call the parent class (Sprite) constructor
@@ -37,31 +38,40 @@ class Asset(pygame.sprite.Sprite):
     def y_vector(self, value):
         self._y_vector = value
 
+    def update(self):
+        # In case it doesnt get over written in the sub classes
+        print "Implement this method"
+
 # Player Sprite class
 class Player(Asset):
-    def __init__(self,image = "no-image"):
+    def __init__(self, image, game, weapon):
         # Call the parent class (Asset) constructor
         Asset.__init__(self, image)
 
         self.health = 3
         self.speed = 7
         self.score = 0
+
+        self.game = game
+
+        # The players deault weapon
+        self.weapon = weapon
         
 
     def move_keyboard(self, key_input, x_max, y_max):
         if key_input['w'] == True:
-            self._y_vector = -7
+            self._y_vector = -1
         
         elif key_input['s'] == True:
-            self._y_vector = 7
+            self._y_vector = 1
         else:
             self._y_vector = 0
 
         if key_input['a'] == True:
-            self._x_vector = -7
+            self._x_vector = -1
         
         elif key_input['d'] == True:
-            self._x_vector = 7
+            self._x_vector = 1
         else:
             self._x_vector = 0
 
@@ -70,20 +80,28 @@ class Player(Asset):
             self._x_vector = 0
         
         elif self.rect.x <= 0 and self._x_vector < 0:
-            self._x = 0
+            self._x_vector = 0
             
         if self.rect.y >= y_max and self._y_vector > 0:
             self._y_vector = 0
         
         elif self.rect.y <= 0 and self._y_vector < 0:
-            self._y = 0
-            
+            self._y_vector = 0
+
+        self._x_vector *= self.speed
+        self._y_vector *= self.speed
+
         self.rect.x += self._x_vector
         self.rect.y += self._y_vector
 
     def hit(self):
         self.health -= 1
-        
+
+    def fire(self):
+        if pygame.time.get_ticks() - self.weapon.last_shot > self.weapon.shot_delay:
+            self.game.bullets.add(self.weapon.fire(self))
+            #print "Fire!"
+            self.weapon.last_shot = pygame.time.get_ticks()
 
 # Top down shooter weapon and bullet class
 class Bullet(Asset):
@@ -115,6 +133,10 @@ class Weapon(Asset):
 
         self.display_width =  pygame.display.get_surface().get_width()
         self.display_height =  pygame.display.get_surface().get_height()
+
+        # weapon fire delay
+        self.shot_delay = 150
+        self.last_shot = 0
     
     # upadate the x and y aim vector of the weapon
     # using the players position and mouse
@@ -136,6 +158,35 @@ class Weapon(Asset):
         # Create a bullet objeect
         bullet = Bullet("images/ball.png", player, self._x_vector, self._y_vector, 1, 9)
         return bullet
+
+# A subclass of the weapon class
+# this weapon fires in a shotgun tpye pattern
+class Weapon_Shotgun(Weapon):
+    def __init__(self, image):
+        Weapon.__init__(self, image)
+
+        # weapon fire delay
+        self.shot_delay = 250
+
+    def fire(self, player):
+        self.update()
+        # Create 3 bullets per shot for the shotgun
+        bullet1 = Bullet("images/ball.png", player, self._x_vector, self._y_vector, 1, 9)
+
+        # Find the vector for the bullets rotated 15 and -15 degrees
+        # This is done using the formula for rotating vectors in a 2D space
+        angle = pi/12
+        
+        x_vec = cos(angle)*self._x_vector - sin(angle)*self._y_vector
+        y_vec = sin(angle)*self._x_vector + cos(angle)*self._y_vector
+        bullet2 = Bullet("images/ball.png", player, x_vec, y_vec, 1, 9)
+
+        x_vec = cos(-angle)*self._x_vector - sin(-angle)*self._y_vector
+        y_vec = sin(-angle)*self._x_vector + cos(-angle)*self._y_vector
+        bullet3 = Bullet("images/ball.png", player, x_vec, y_vec, 1, 9)
+
+        # return the 3 bullets as a list
+        return [bullet1, bullet2, bullet3]
 
 class Enemy(Asset):
     # Initialize enemy with player object so the enemy can track the player
@@ -171,21 +222,73 @@ class Enemy(Asset):
     # this will lower the health and destroy the object if need be
     def hit(self, damage):
         self.health -= damage
-        if self.health <= 0:
-            self.kill()
-            self.player.score += 1
-            print "Enemy Killed"
         
 
-# Class for game powerups
-class PowerUp(Asset):
-    def __init__(self, image):
+# Health powerup class 
+class PowerUp_Health(Asset):
+    def __init__(self, image, x, y, player):
         Asset.__init__(self, image)
+        
+        self.rect.x = x
+        self.rect.y = y
 
+        self.player = player
 
+        self.time = 0
 
+    def end(self):
+        self.kill()
+    
+    # use the power up
+    def use(self):
+        #increase player health by 1
+        self.player.health += 1
 
+# Speed Boost class 
+class PowerUp_Speed(Asset):
+    def __init__(self, image, x, y, player):
+        Asset.__init__(self, image)
+        
+        self.rect.x = x
+        self.rect.y = y
 
+        self.player = player
 
+        # last for 10 seconds
+        self.time = 10000
 
+    def end(self):
+        self.player.speed /= 2
+        self.kill()
+            
+    def use(self):
+        self.player.speed *= 2
+            
+    
+
+# Weapon Boost class
+# hope you like bullets :)
+class PowerUp_Weapon(Asset):
+    def __init__(self, image, weapon , x, y, player):
+        Asset.__init__(self, image)
+        
+        self.rect.x = x
+        self.rect.y = y
+
+        self.player = player
+    
+        self.time = 10000
+
+        self.weapon_upgrade = weapon
+        
+        # keep track of the player's default weapon
+        self.weapon_default = self.player.weapon
+
+    def end(self):
+        self.player.weapon = self.weapon_default
+        self.kill()
+        
+    # use the power up
+    def use(self):
+        self.player.weapon = self.weapon_upgrade
         
--- Top Down Shooter/game.py
+++ Top Down Shooter/game.py
@@ -5,14 +5,18 @@ from random import randint
 #initialize pygame
 pygame.init()
 
+#initialize the pygame sound mixer
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
+
 myFont = pygame.font.SysFont("monospace", 15)
         
 # class for the actual game        
 class Game(object):
-    def __init__(self, map, player, width, height):
+    def __init__(self, map, width, height):
         # Groups to store all the enemies and game data during the current wave
         self.enemies = pygame.sprite.Group()
         self.bullets = pygame.sprite.Group()
+        self.powerups = pygame.sprite.Group()
 
         # width and height of the display window
         self.width = width
@@ -26,7 +30,14 @@ class Game(object):
         # Store the map to play with and the player to use
         self.map = map
         self.temp_map = self.map.copy()
-        self.player = player
+        
+        # Create the different weapon objects
+        self.machine_gun = Weapon("images/fire-bolt.png")
+        self.shotgun = Weapon_Shotgun("images/fire-bolt.png")
+        
+        # Create a player object of some soup and use the machine gun as
+        # the default weapon
+        self.player =  Player("images/soup.png", self, self.machine_gun)
 
         # Map offsets since (0,0) on the display is top left and not center
         # these offset the map based off the size of the display
@@ -44,17 +55,20 @@ class Game(object):
         #set crashed state variable to false
         self.crashed = False
 
-        # Create a weapon object
-        self.blaster = Weapon("images/fire-bolt.png")
-        # weapon fire delay
-        # move these to the weapon class
-        self.shot_delay = 150
-        self.last_shot = 0
-
         # Game variables
         self.wave_number = 1
-        self.inventory = 0
-        self.active_powerup = 0
+        self.inventory = None
+        self.active_powerup = None
+        self.powerup_timer = 0
+
+        # Sound objects
+        self.ding = pygame.mixer.Sound("sounds/ding.wav")
+        self.hitmarker = pygame.mixer.Sound("sounds/hitmarker.wav")
+        self.pew = pygame.mixer.Sound("sounds/pew.wav")
+         
+
+        # variable to keep track of what screen to display
+        self.screen = "game"
 
     # Function to setup the game
     def setup(self):
@@ -88,6 +102,15 @@ class Game(object):
         for enemy, b in collisions.iteritems():
             for bullet in b:
                 enemy.hit(bullet.damage)
+                if enemy.health <= 0:
+                    enemy.kill()
+                    self.player.score += 1
+                    self.ding.play()
+                    print "Enemy Killed"
+                    # roll for a random power up drop
+                    # have a 25% chance of a powerup drop
+                    self.roll_powerup(25,enemy)
+                
 
         # Check for enemy-player collision
         # remove the enemy upon collision
@@ -95,6 +118,30 @@ class Game(object):
 
         for enemy in collisions:
             self.player.health -= 1
+            self.hitmarker.play()
+
+        # Check for player-powerup collision
+        collisions = pygame.sprite.spritecollide(self.player, self.powerups, False)
+
+        # update the dropped powerups on the screen
+        for p in self.powerups:
+            self.temp_map.blit(p.image, (p.rect.x, p.rect.y))
+        
+        # Add powerup to enventory if not carrying one
+        for p in collisions:
+            if self.inventory == None:
+                self.inventory = p
+                self.powerups.remove(p)
+
+        # If there is an active powerup
+        if not self.active_powerup == None:
+            # If the power up has run out of time
+            if pygame.time.get_ticks() - self.powerup_timer > self.active_powerup.time:
+                # End the powerup
+                print "PowerUp ended"
+                self.active_powerup.end()
+                self.active_powerup = None
+                
 
                 
 
@@ -144,18 +191,12 @@ class Game(object):
                     elif event.button == 3:
                         self.input_map['right_click'] = False
                         
-    # fire the player weapon
-    def fire_weapon(self):
-        if pygame.time.get_ticks() - self.last_shot > self.shot_delay:
-            self.bullets.add(self.blaster.fire(self.player))
-            #print "Fire!"
-            self.last_shot = pygame.time.get_ticks()
             
     # spawn a single enemy at a random map location
     # given the enemy a random speed and random health
     def spawn_enemy(self):
         # Choose a random enemy speed
-        speed = randint(2, 5)
+        speed = randint(2, 4)
         # choose a random enemy health
         health = randint(1, 3)
 
@@ -167,19 +208,60 @@ class Game(object):
 
         print "Enemy Spawned"
 
+    # Rolls for a random chance of a powerup
+    # use a whole number percentage for chance (do not put the percent sign)
+    def roll_powerup(self, chance, enemy):
+        rand_num = randint(0,100)
+
+        if rand_num < chance:
+            self.spawn_powerup(enemy.rect.x, enemy.rect.y)
+
+    # Spawn a powerup at a location
+    def spawn_powerup(self, x, y):
+        # choose a random number
+        rand_num = randint(1,3)
+
+        # Spawn one of 3 powerups based off that random number
+        if rand_num == 1:
+            self.powerups.add(PowerUp_Health("images/health.png", x, y, self.player))
+
+        elif rand_num == 2:
+            self.powerups.add(PowerUp_Speed("images/speed.png", x, y, self.player))
+
+        else:
+            self.powerups.add(PowerUp_Weapon("images/nuke.png", self.shotgun, x, y, self.player))
+
+    # Activate the powerup in the player inventory
+    def activate_powerup(self):
+        self.active_powerup = self.inventory
+        self.inventory = None
+        self.active_powerup.use()
+
+        self.powerup_timer = pygame.time.get_ticks()
+        print "PowerUp Used"
+        
     # Display game hud
     # show the health, current wave, and the score
     def game_hud(self):
         health = myFont.render("Health: " + str(self.player.health), 1, (255,255,255), (0,0,0))
         wave = myFont.render("Wave: " + str(self.wave_number), 1, (255,255,255), (0,0,0))
         score = myFont.render("Score: " + str(self.player.score), 1, (255,255,255), (0,0,0))
+        powerup = myFont.render("PowerUp: ", 1, (255,255,255), (0,0,0))
 
         self.window.blit(health, (10,10))
         self.window.blit(score, (self.width-100, 10))
         self.window.blit(wave, (self.width-100, self.height - 20))
 
-        
+        if self.inventory:
+            self.window.blit(powerup, (self.width/2 - 20, 15))
+            self.window.blit(self.inventory.image, (self.width/2 + 50, 5))
 
+        if self.active_powerup:
+            time_left = self.active_powerup.time - (pygame.time.get_ticks() - self.powerup_timer)
+            timer = myFont.render("PowerUp: " + str(time_left), 1, (255,255,255), (0,0,0))
+            
+            self.window.blit(timer, (self.width/2, 25))
+    
     # Play a wave                     
     def play_wave(self, wave_number):
         enemies_to_spawn = wave_number ** (1.5)
@@ -188,7 +270,8 @@ class Game(object):
 
         print "Wave {} Started".format(wave_number)
         
-        while(enemies_to_spawn > 0 or len(self.enemies) > 0):
+        while(True):
+            
             if (pygame.time.get_ticks() - last_spawn > spawn_delay) and enemies_to_spawn > 0:
                 self.spawn_enemy()
                 spawn_delay = randint(1000, 3000)
@@ -199,23 +282,30 @@ class Game(object):
             # check for inputs
             self.check_events()
 
+            # Move the player using keyboard inputs
+            self.player.move_keyboard(self.input_map, self.map.get_width(), self.map.get_height())
+            
             # create a surface of only a portion of the map based on player position
             # apply the offsets to the map_view image to have it centered correctly
             self.map_view = pygame.Surface((self.width, self.height))
+            
             self.update()
             
             self.map_view.blit(self.temp_map, (0,0), ((self.player.rect.x - self.x_offset), \
                                                  (self.player.rect.y - self.y_offset),\
                                                  self.width, self.height))
 
-            self.player.move_keyboard(self.input_map, self.map.get_width(), self.map.get_height())
-
             if self.input_map['left_click'] == True:
-                self.fire_weapon()
+                self.player.fire()
+                self.pew.play()
+                #self.fire_weapon()
+            if self.input_map['right_click'] == True and self.inventory != None \
+               and self.active_powerup == None:
+                self.activate_powerup()
 
             # print the background then the player in the center on top of the background
             self.window.blit(self.map_view, (0,0))
-            self.window.blit(soup.image, (self.width/2 - self.player.width/2, \
+            self.window.blit(self.player.image, (self.width/2 - self.player.width/2, \
                                           self.height/2 - self.player.height/2))
 
             # Display game hud
@@ -224,25 +314,71 @@ class Game(object):
             # update the pygame display    
             pygame.display.update()
 
+            # Check for conditions to end the wave
+            if enemies_to_spawn <= 0 and len(self.enemies) <= 0:
+                self.wave_number += 1
+                break
+
+            elif self.player.health <= 0:
+                self.screen = "lose_screen"
+                break
+
             # limit the game to 60 fps
             self.clock.tick(60)
 
         print "Wave {} Completed".format(wave_number)
-        
-            
+    
+    # Main Menu screen
+    def main_menu(self):
+        print "add main menu code"
+
+    # Lose screen
+    def lose_screen(self):
+        print "Add lose screen code"
+
+
+    # level up screen
+    def level_up_screen(self):
+        print "add level up screen code"
+
+    # credit screen
+    def credit_screen(self):
+        print "add credit screen code"
+
+    # highscores screen
+    def highscores_screen(self):
+        print "add highscores screen code"
 
     # Function to actually play the game
     def play(self):
         test_timer = 0
         while not self.crashed:
-            if self.player.health > 0:
-                self.play_wave(self.wave_number)
-                self.wave_number += 1
-            else:
-                print "Player Dead"
-                break
 
-        pygame.quit()
+            # Check for which game screen or menu to display
+            if self.screen == "game":
+                self.play_wave(self.wave_number)
+
+            elif self.screen == "main_menu":
+                self.main_menu()
+
+            elif self.screen == "lose_screen":
+                self.lose_screen()
+
+            elif self.screen == "level_up_screen":
+                self.level_up_screen()
+
+            elif self.screen == "credit_screen":
+                self.credit_screen()
+
+            elif self.screen == "highscores_screen":
+                self.highscores_screen()
+                
+            else:
+                pass
+
+            self.clock.tick(60)
+
+        #pygame.quit()
         quit()
 
 
@@ -256,29 +392,14 @@ HEIGHT = 600
 # map object
 play_map = pygame.image.load("images/background.png")
 
-# Create a player object of some soup
-soup =  Player("images/soup.png")
-
 # Create the game object
-game = Game(play_map, soup, WIDTH, HEIGHT)
+game = Game(play_map, WIDTH, HEIGHT)
 
 # Setup the game
 game.setup()
 
 # run the game
 game.play()
-
-
-##try:
-##    while(True):
-##
-##        #RUN CODE HERE
-##
-##
-##
-##except KeyboardInterrupt:
-##    # reset the GPIO pins
-##    GPIO.cleanup()
 
 
 
